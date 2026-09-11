ae6298d0b3615cb32b229b537a4c486597a9e4d3
ayman <thxmxxmxshrxf@gmail.com>
2026-09-11 10:48:14 +0530

    fix(tests): await async user save

--- tests/userService.test.js
+++ tests/userService.test.js
@@ -1,7 +1,11 @@
-const { saveUser, getUsers } = require('../src/userService');
+const { saveUser, getUsers, clearUsers } = require('../src/userService');
 
-test('saves a user to the service', () => {
-  saveUser({ name: 'Alice' });
+beforeEach(() => {
+  clearUsers();
+});
+
+test('saves a user to the service', async () => {
+  await saveUser({ name: 'Alice' });
   expect(getUsers().length).toBe(1);
 });
 
