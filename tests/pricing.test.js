12a562b6cf8a707b9c1dbfd16a74ff42c5952500
ayman <thxmxxmxshrxf@gmail.com>
2026-09-11 10:48:34 +0530

    fix(tests): make discount test deterministic

--- tests/pricing.test.js
+++ tests/pricing.test.js
@@ -1,10 +1,16 @@
 const { calculateDiscount } = require('../src/pricing');
 
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+
 test('calculateDiscount returns a number', () => {
+  jest.spyOn(Math, 'random').mockReturnValue(0.5);
   expect(typeof calculateDiscount()).toBe('number');
 });
 
 test('discount is above minimum threshold', () => {
+  jest.spyOn(Math, 'random').mockReturnValue(0.5);
   const discount = calculateDiscount();
   expect(discount).toBeGreaterThan(0.05);
 });
