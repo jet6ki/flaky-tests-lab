cfa7e6b6449a5c896e8be1dd4da0667df87e4213
ayman <thxmxxmxshrxf@gmail.com>
2026-09-11 11:01:01 +0530

    Update cart.test.js

--- tests/cart.test.js
+++ tests/cart.test.js
@@ -1,20 +1,16 @@
-const cart = require('../src/cart');
-
-beforeEach(() => {
-  cart.clearCart();
-});
+const { addItem, getCart, removeItem } = require('../src/cart');
 
 test('addItem adds to cart', () => {
-  cart.addItem({ id: 1, name: 'Widget' });
-  expect(cart.getCart().length).toBe(1);
+  addItem({ id: 1, name: 'Widget' });
+  expect(getCart().length).toBe(1);
 });
 
 test('cart starts empty', () => {
-  expect(cart.getCart().length).toBe(0);
+  expect(getCart().length).toBe(0);
 });
 
 test('removeItem removes from cart', () => {
-  cart.addItem({ id: 2, name: 'Gadget' });
-  cart.removeItem(2);
-  expect(cart.getCart().length).toBe(0);
+  addItem({ id: 2, name: 'Gadget' });
+  removeItem(2);
+  expect(getCart().length).toBe(0);
 });
